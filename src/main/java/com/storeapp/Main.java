06b14310f3e4304d4ef5df56132c253c2dfced83
Matthew <mgmaingot@gmail.com>
2025-11-29 17:26:56 +0100

    refactor: use SceneUtil for initial scene setup

--- src/main/java/com/storeapp/Main.java
+++ src/main/java/com/storeapp/Main.java
@@ -1,26 +1,15 @@
 package com.storeapp;
 
+import com.storeapp.util.SceneUtil;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+    public void start(Stage primaryStage) {
+        SceneUtil.switchScene(primaryStage, "/fxml/login.fxml");
         primaryStage.setTitle("Retail Store Management System");
-        
-        // Get screen bounds for full-screen experience
-        javafx.stage.Screen screen = javafx.stage.Screen.getPrimary();
-        javafx.geometry.Rectangle2D bounds = screen.getVisualBounds();
-        
-        Scene scene = new Scene(root, bounds.getWidth(), bounds.getHeight());
-        scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
-        
-        primaryStage.setScene(scene);
         primaryStage.setMaximized(true);
         primaryStage.setResizable(true);
         primaryStage.show();
